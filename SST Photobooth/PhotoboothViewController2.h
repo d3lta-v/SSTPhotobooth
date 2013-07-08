ae7016158b9e675b926a356c5e2ad100f5323c95
Pan Ziyue <random.rrr3@gmail.com>
2013-07-08 15:06:13 +0800

    UI changes

---
+++ SST Photobooth/PhotoboothViewController2.h
@@ -0,0 +1,13 @@
+//
+//  PhotoboothViewController2.h
+//  SST Photobooth
+//
+//  Created by Pan Ziyue on 4/7/13.
+//  Copyright (c) 2013 Pan Ziyue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PhotoboothViewController2 : UIViewController
+
+@end
---
+++ SST Photobooth/PhotoboothViewController2.m
@@ -0,0 +1,38 @@
+//
+//  PhotoboothViewController2.m
+//  SST Photobooth
+//
+//  Created by Pan Ziyue on 4/7/13.
+//  Copyright (c) 2013 Pan Ziyue. All rights reserved.
+//
+
+#import "PhotoboothViewController2.h"
+
+@interface PhotoboothViewController2 ()
+
+@end
+
+@implementation PhotoboothViewController2
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
