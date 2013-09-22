e017ecacf3c4a5cd4af1fea1407bc92cfa152b92
Pan Ziyue <random.rrr3@gmail.com>
2013-09-22 23:40:07 +0800

    fixed some storyboard problems (with status bar appearing and disappearing)

---
+++ SST Photobooth/LastTutorialViewController.h
@@ -0,0 +1,13 @@
+//
+//  LastTutorialViewController.h
+//  SST Lens
+//
+//  Created by Pan Ziyue on 22/9/13.
+//  Copyright (c) 2013 Pan Ziyue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LastTutorialViewController : UIViewController
+
+@end
---
+++ SST Photobooth/LastTutorialViewController.m
@@ -0,0 +1,43 @@
+//
+//  LastTutorialViewController.m
+//  SST Lens
+//
+//  Created by Pan Ziyue on 22/9/13.
+//  Copyright (c) 2013 Pan Ziyue. All rights reserved.
+//
+
+#import "LastTutorialViewController.h"
+
+@interface LastTutorialViewController ()
+
+@end
+
+@implementation LastTutorialViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (BOOL)prefersStatusBarHidden
+{
+    return YES;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
