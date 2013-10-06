ba3d07f833a9d0b61dd02868bc68c88d03843a8f
Pan Ziyue <random.rrr3@gmail.com>
2013-10-06 22:41:32 +0800

    edited a small amount of code for the intensity of the filters

--- SST Photobooth/PhotoboothViewController2.m
+++ SST Photobooth/PhotoboothViewController2.m
@@ -95,6 +95,8 @@
             [SVProgressHUD showWithStatus:@"Applying filter..."];
             GPUImageEmbossFilter *filter = [[GPUImageEmbossFilter alloc] init];
             
+            [filter setIntensity:0.7];
+            
             double delayInSeconds = 0.1;
             dispatch_time_t popTime = dispatch_time(DISPATCH_TIME_NOW, (int64_t)(delayInSeconds * NSEC_PER_SEC));
             dispatch_after(popTime, dispatch_get_main_queue(), ^(void){
